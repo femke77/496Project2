8b205e79e0459ff0db04445d066f877ff06d8b37
femke77 <megan.meyers.388@my.csun.edu>
2017-03-20 15:44:13 -0700

    skelton

--- src/project2/JobScheduler.java
+++ src/project2/JobScheduler.java
@@ -3,78 +3,72 @@ package project2;
 import java.util.ArrayList;
 
 public class JobScheduler {
-	
+
 	private int nJobs = 0;
 	private Job[] jobs;
-	
-	public JobScheduler(int[] joblength, int[] deadline, int[] profit){
-		
+
+	public JobScheduler(int[] joblength, int[] deadline, int[] profit) {
+
 		nJobs++;
-		
+
 	}
-    
-	public void printJobs(){
-		
-		
+
+	public void printJobs() {
+
 	}
-	
-	//insert 5 methods for making schedules 
-	
-	
+
+	// insert 5 methods for making schedules
+
 	public static void main(String[] args) {
-		
 
 	}
 
-		
-	
-	
-public class Job {	
-	
-	int jobNumber, length, deadline, profit, start, finish;
-	
-	public Job(int jn, int len, int d, int p){
-		
-		jobNumber = jn;
-		length = len;
-		deadline = d;
-		profit = p;
-		start = -1;
-		finish = -1;
-	}
-	
-   public String toString(){
-	   return "#" + jobNumber + ":("+ length + ","+ deadline + ","+profit + "," + start + ","+ finish + ")";
-   }
-	
-}//end class Job
+	public class Job {
 
-public class Schedule {
-	
-	ArrayList<Job> schedule;
-	int profit;
-	
-	public Schedule(){
-		
-		profit = 0;
-		schedule = new ArrayList<Job>();
-	}
-	
-	public void add(Job job){
-		
-	}
-	
-	public int getProfit(){
-		return profit;
-	}
-	
-	public String toString(){
-		String s = "Schedule profit = "+ profit;
-		for (int k = 0; k < schedule.size(); k++){
-			s = s + "\n" + schedule.get(k);
+		int jobNumber, length, deadline, profit, start, finish;
+
+		public Job(int jn, int len, int d, int p) {
+
+			jobNumber = jn;
+			length = len;
+			deadline = d;
+			profit = p;
+			start = -1;
+			finish = -1;
 		}
-		return s;
-	}
-}//end class Schedule
-	
-}//end main class JobScheduler
+
+		public String toString() {
+			return "#" + jobNumber + ":(" + length + "," + deadline + "," + profit + "," + start + "," + finish + ")";
+		}
+
+	}// end class Job
+
+	public class Schedule {
+
+		ArrayList<Job> schedule;
+		int profit;
+
+		public Schedule() {
+
+			profit = 0;
+			schedule = new ArrayList<Job>();
+		}
+
+		public void add(Job job) {
+
+		}
+
+		public int getProfit() {
+			return profit;
+		}
+
+		public String toString() {
+			String s = "Schedule profit = " + profit;
+			for (int k = 0; k < schedule.size(); k++) {
+				s = s + "\n" + schedule.get(k);
+			}
+			return s;
+		}
+	}// end class Schedule
+
+}// end main class JobScheduler
